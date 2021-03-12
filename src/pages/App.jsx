38476a103e6581e22cb3e043032b0bfcfb1ba9c0
lalitsharma <lalitsecret@gmail.com>
2021-03-12 14:11:12 +0530

    message

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import Login from './Login'
+import Admin from './admin/App'
 function App()
 {
-	return <div>App</div>
+	return <BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={Login} />
+			<Route exact path="/admin/:x" component={Admin} />
+		</Switch>
+	</BrowserRouter>
 }
 export default App
