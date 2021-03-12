fd2739e2688be15229648f1549a34306c8b6239c
lalitsharma <lalitsecret@gmail.com>
2021-03-12 15:57:05 +0530

    message4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
-function Login()
+import {useSelector,useDispatch} from 'react-redux'
+function Login(props)
 {
-	return <div>Login</div>
+	let state=useSelector(s=>s)
+	let dispatch=useDispatch()
+	let [ob,setob]=React.useState({email:"",password:""})
+	return <div>
+		<div className="login">
+			<h3>Login user</h3>
+			<p>Email</p>
+			<input placeholder="email" value={ob.email} onChange={e=>setob({...ob,email:e.target.value})}/>
+			<p>Password</p>
+			<input placeholder="password" value={ob.password} onChange={e=>setob({...ob,password:e.target.value})}/>
+			<button onClick={e=>login('admin')}>admin</button>
+			<button onClick={e=>login('faculty')}>faculty</button>
+			<button onClick={e=>login('student')}>student</button>
+		</div>
+	</div>
 }
 export default Login
