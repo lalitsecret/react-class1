0b053100db7cdbd735c6596c7750c9149dff8db7
lalitsharma <lalitsecret@gmail.com>
2021-03-12 14:30:32 +0530

    message

--- src/pages/admin/Header.jsx
+++ src/pages/admin/Header.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 function Header()
 {
-	return <div>Header</div>
+	return <header>
+		<div className="item">
+			<h1>Admin Panel</h1>
+		</div>
+		<div className="item">
+			<Link to="/Admin/Index">Home</Link>
+			<Link to="/Admin/Admin">Admin</Link>
+			<Link to="/Admin/Faculty">Faculty</Link>
+			<Link to="/Admin/Student">Student</Link>
+			<Link to="/Admin/Courses">Courses</Link>
+			<Link to="/Admin/Slots">Slots</Link>
+			<Link to="/Admin/Batch">Batch</Link>
+			<Link to="/Admin/Chart">Chart</Link>
+			<Link to="/Admin/Graph">Graph</Link>
+			<Link to="/Admin/Planner">Planner</Link>
+			<Link to="/Admin/Logout">Logout</Link>
+		</div>
+		
+	</header>
 }
 export default Header
